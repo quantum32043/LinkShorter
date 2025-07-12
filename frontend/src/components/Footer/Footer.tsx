a21e0cb42b5f36ef21517d43a407404bbccad9dc
quantum32043 <quantum32043@gmail.com>
2025-07-13 01:25:46 +0300

    Added Footer.tsx and Header.tsx components templates

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import {render} from "@testing-library/react";
+
+const Footer: React.FC = () => {
+    return(
+        <footer>
+            <div className="footer">
+
+            </div>
+        </footer>
+    );
+};
+
+
+export default Footer;

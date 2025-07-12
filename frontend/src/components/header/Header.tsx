be82d7d6a44e5904fd30ac3ab7de33f3446e0e83
quantum32043 <quantum32043@gmail.com>
2025-07-13 01:25:46 +0300

    Added Footer.tsx and Header.tsx components templates

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return(
+        <header>
+            <div className="logo">
+                <img src="" alt=""/>
+            </div>
+        </header>
+    );
+};
+
+
+export default Header;

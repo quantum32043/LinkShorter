4275e64a39dfacfe16542deb1d9ff7d9fa434849
quantum32043 <quantum32043@gmail.com>
2025-07-31 03:02:06 +0300

    Finalize Header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,23 @@
+const Header = () => {
+    return(
+        <div className="w-full h-auto px-[47px] py-[40px] bg-transparent fixed">
+            <div className="h-[60px] flex flex-row items-center justify-between">
+                <a className="text-transparent text-[37px] font-sans font-bold bg-clip-text bg-gradient-to-r from-blue-700 via-pink-500 to-blue-700">Shorter</a>
+                <div className="w-[322px] h-full flex justify-between items-center">
+                    <button
+                        type={"button"}
+                        className="w-[125px] h-full border-[#353C4A] border-1 rounded-[48px] bg-[#181E29] font-bold font-sans text-white text-[16px]">
+                        Login
+                    </button>
+                    <button
+                        type={"button"}
+                        className="w-[180px] h-full border-[#144EE3] border-1 rounded-[48px] bg-[#144EE3] font-bold font-sans text-white text-[16px]">
+                        Register Now
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header

2cd650a2875176ac918a8d2f094eb9015740edd0
quantum32043 <quantum32043@gmail.com>
2025-08-08 02:49:58 +0300

    fix authstore actions add header logout button

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -1,16 +1,12 @@
 import {api} from "./axiosInstance.ts";
 
 export const authApi = {
-    login: (email: string, password: string) => {
-        api.post("/login", { email, password })
-    },
-    register: (email: string, password: string) => {
-        api.post("/register", { email, password })
-    },
-    logout: () => {
-        api.post("/logout")
-    },
-    refresh: () => {
+    login: (email: string, password: string) =>
+        api.post("/login", { email, password }),
+    register: (email: string, password: string) =>
+        api.post("/register", { email, password }),
+    logout: () =>
+        api.post("/logout"),
+    refresh: () =>
         api.post("/refresh")
-    }
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import {observer} from "mobx-react-lite";
 import {modalStore} from "../store/ModalStore.ts";
+import {authApi} from "../api/authApi.ts";
+import {authStore} from "../store/AuthStore.ts";
 
 const Header = observer(() => {
     return(
@@ -9,6 +11,7 @@ const Header = observer(() => {
                 <div className="h-[60px] flex flex-row items-center justify-between">
                     <a href={"#"}
                        className="text-transparent text-[37px] font-sans font-bold bg-clip-text bg-gradient-to-r from-blue-700 via-pink-500 to-blue-700">Shorter</a>
+                    {authStore.isAuthorized ? (
                     <div className="w-[322px] h-full flex justify-between items-center">
                         <button
                             type={"button"}
@@ -27,6 +30,20 @@ const Header = observer(() => {
                             Register Now
                         </button>
                     </div>
+                    ) : (
+                        <button
+                            type={"button"}
+                            onClick={async () => {
+                                try {
+                                    await authStore.logout()
+                                } catch (e) {
+                                    console.error(e)
+                                }
+                            }}
+                            className="w-[180px] h-full border-[#144EE3] border-1 rounded-[48px] bg-[#144EE3] font-bold font-sans text-white text-[16px]">
+                            Logout
+                        </button>
+                    )}
                 </div>
             </header>
         </>

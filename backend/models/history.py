d4a20a4ab0d294fbb48a8b32bc3fae3b015e325f
quantum32043 <quantum32043@gmail.com>
2025-08-21 01:50:37 +0300

    add sqlalchemy models

---
+++ backend/models/history.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from backend.models.base import Base
+from backend.models.user import User
+
+
+class History(Base):
+    __tablename__ = 'history'
+
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    original_link: Mapped[str] = mapped_column()
+    short_link: Mapped[str] = mapped_column()
+    date: Mapped[datetime] = mapped_column()
+
+    user: Mapped[User] = relationship(back_populates="history")
+
+    def __repr__(self) -> str:
+        return (f"History(id={self.id!r}, user_id={self.user_id!r}, original_link={self.original_link!r}, "
+                f"short_link={self.short_link!r} date={self.date!r})")
